90fab05389a7402c58b94d1fbf3da7da5dce92ec
= <=>
2024-02-12 15:29:47 +0530

    add healthchech controlller and subscribtion model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,16 +2,16 @@ import mongoose, { Schema } from "mongoose";
 
 const subscriptionSchema = new Schema(
   {
-    subcriber: {
-      type: Schema.Types.ObjectId,
+    subscriber: {
+      type: Schema.Types.ObjectId, // one who is subscribing
       ref: "User",
     },
     channel: {
-      type: Schema.Types.ObjectId,
+      type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
       ref: "User",
     },
   },
   { timestamps: true }
 );
 
-export const Subscription = mongoose.model("Subcription", subscriptionSchema);
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
