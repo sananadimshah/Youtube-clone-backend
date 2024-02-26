22a98d8769e9ee3fa0c9bab9e6be4d75f8d5f3c4
= <=>
2024-02-26 21:04:47 +0530

    completed Subscription Controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -14,27 +14,141 @@ const toggleSubscription = asyncHandler(async (req, res) => {
   if (!mongoose.isValidObjectId(channelId)) {
     throw new ApiError(400, "Invalid channelId");
   }
-  const findSubscription = await Subscription.findOne({ channel: channelId });
-  if (findSubscription) {
-    await Subscription.deleteOne({ channelId });
-    return res.status(200).json(200, "Successfully Unsubscrided");
-  }
-  if (!findSubscription) {
-    await Subscription.create({ channel: channelId, subscriber: req.user._id });
-    return res
-      .status(201)
-      .json(new ApiResponse(200, "Successfully Unsubscrided"));
+
+  try {
+    const findSubscription = await Subscription.findOne({ channel: channelId });
+    if (findSubscription) {
+      await Subscription.deleteOne({ channel: channelId });
+      return res
+        .status(200)
+        .json({ status: true, msg: "SuccessFully unSubscrided Channel" });
+    }
+    console.log(findSubscription);
+    if (!findSubscription) {
+      const subscribe = await Subscription.create({
+        channel: channelId,
+        subscriber: req.user._id,
+      });
+      return res
+        .status(201)
+        .json(
+          new ApiResponse(200, subscribe, "Successfully Channel Subscride")
+        );
+    }
+  } catch (error) {
+    throw new ApiError(500, "Something went wrong");
   }
 });
 
-// controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-  const { channelId } = req.params;
+  let { channelId } = req.params;
+  console.log(channelId);
+  if (!channelId) {
+    throw new ApiError(400, "Please provide channelId");
+  }
+  if (!mongoose.isValidObjectId(channelId)) {
+    throw new ApiError(400, "Invalid channelId");
+  }
+  channelId = new mongoose.Types.ObjectId(channelId);
+  try {
+    const subscribers = await Subscription.aggregate([
+      {
+        $match: { channel: channelId },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "subscriber",
+          foreignField: "_id",
+          as: "subscriberInfo",
+        },
+      },
+
+      {
+        $unwind: "$subscriberInfo",
+      },
+
+      {
+        $project: {
+          _id: 0,
+          subscriber: {
+            fullName: "$subscriberInfo.fullName",
+            email: "$subscriberInfo.email",
+            username: "$subscriberInfo.username",
+            avatar: "$subscriberInfo.avatar",
+          },
+        },
+      },
+    ]);
+    if (!subscribers.length) {
+      throw new ApiError(404, "No subcriber found with this Id");
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(
+          200,
+          subscribers,
+          "Successfully Fetch Subscribed Channel "
+        )
+      );
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-  const { subscriberId } = req.params;
+  let { subscriberId } = req.params;
+
+  if (!subscriberId) {
+    throw new ApiError(400, "Please provide subscriberId");
+  }
+  if (!mongoose.isValidObjectId(subscriberId)) {
+    throw new ApiError(400, "Invalid subscriberId");
+  }
+  subscriberId = new mongoose.Types.ObjectId(subscriberId);
+  try {
+    const channel = await Subscription.aggregate([
+      {
+        $match: { subscriber: subscriberId },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "channel",
+          foreignField: "_id",
+          as: "channelInfo",
+        },
+      },
+
+      {
+        $unwind: "$channelInfo",
+      },
+
+      {
+        $project: {
+          _id: 0,
+          channel: {
+            fullName: "$channelInfo.fullname",
+            email: "$channelInfo.email",
+            username: "$channelInfo.username",
+            avatar: "$channelInfo.avatar",
+          },
+        },
+      },
+    ]);
+    if (!channel.length) {
+      throw new ApiError(404, "No subscrided channel found");
+    }
+    return res
+      .status(200)
+      .json(
+        new ApiResponse(200, channel, "Successfully Fetch Subscribed Channel")
+      );
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
